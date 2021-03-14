027fba3f247376efd7269f300513d7c81d6387ed
hithaprani <hithaprani.vakkanti@popcornapps.com>
2021-03-14 17:19:04 +0530

    minor fixes

--- template.config.js
+++ template.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  // Placeholder used to rename and replace in files
-  // package.json, index.json, android/, ios/
   placeholderName: "basicTemplateApp",
-  // Directory with template
   templateDir: "./basicTemplateApp",
-  // Path to script, which will be executed after init
   postInitScript: "./script.js",
 };
